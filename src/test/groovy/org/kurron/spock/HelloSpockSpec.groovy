133ec43db5153a509c86f52ef7e5223da2ed5b13
kurron <kurr@kurron.org>
2012-09-25 09:52:12 -0400

    Added more examples.

--- src/test/groovy/org/kurron/spock/HelloSpockSpec.groovy
+++ src/test/groovy/org/kurron/spock/HelloSpockSpec.groovy
@@ -1,5 +1,7 @@
 package org.kurron.spock
 
+import spock.lang.Shared
+
 /*
  * Copyright [2012] [Ronald D. Kurr]
  *
@@ -17,14 +19,78 @@ package org.kurron.spock
  */
 
 class HelloSpockSpec extends spock.lang.Specification {
-    def "length of Spock's and his friends' names"() {
-        expect:
-        name.size() == length
+    /* ---------------------- fields ------------------*/
+    def notSharedBetweenFeatures = Integer.toString( new Random().nextInt( Integer.MAX_VALUE ), 16 )
+
+    @Shared
+    def sharedBetweenFeatures = Integer.toString( new Random().nextInt( Integer.MAX_VALUE ), 16 )
+
+    // statics should be used for constants only
+    static final PI = 3.141592654
+
+    /*-------------------------- fixture methods -----------------*/
+    def setup() { println '    -------- setup ----------' }
+    def cleanup()
+    {
+        println '    -------- cleanup ----------'
+        println()
+    }
+
+    def setupSpec() { println '---- uber setup ----'}
+    def cleanupSpec() { println '---- uber clean up ----'}
+
+    /*------------------------- feature methods -------------------------*/
+
+    def "computing the maximum of two numbers"() {
+        setup: "open database connection"
+            println '        specification one: Setup phase'
+            println '        specification one: notSharedBetweenFeatures = ' + notSharedBetweenFeatures
+            println '        specification one: sharedBetweenFeatures = ' + sharedBetweenFeatures
+
+        when:
+            println '        specification one: Stimulus phase'
+
+        then:
+            println '        specification one: Response phase'
+            println '        a = ' + a + ', b = ' + ', c = ' + c
+            Math.max(a, b) == c
+
+        cleanup:
+            println '        specification one: Cleanup phase'
 
         where:
-        name     | length
-        "Spock"  | 5
-        "Kirk"   | 4
-        "Scotty" | 1
+            a << [5, 3]
+            b << [1, 9]
+            c << [5, 9]
+    }
+
+    def "specification two"() {
+        given: "an empty bank account"
+            println '        specification two: an empty bank account'
+            println '        specification two: notSharedBetweenFeatures = ' + notSharedBetweenFeatures
+            println '        specification one: sharedBetweenFeatures = ' + sharedBetweenFeatures
+
+        and: "and empty credit account"
+            println '        specification two: and empty credit account'
+
+        when: "the account is credited ten dollars"
+            println '        specification two: the account is credited ten dollars'
+
+        then:  "the account's balance is ten dollars"
+            println '        specification two: the account balance is ten dollars'
+            'a' == 'a'
+        and: "the credit account is empty"
+            helperMethodsShouldReturnVoid()
+
+        cleanup:
+            println '        specification two: Cleanup phase'
+    }
+
+    /*------------------------- helper methods --------------------------*/
+
+    def void helperMethodsShouldReturnVoid()
+    {
+        println '        specification two: the credit account is empty'
+        assert 0 == 0
     }
 }
